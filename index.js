35aec94ebaca03619898506351e3e9ea8314c752
Alvaro <apatsan098@g.educaand.es>
2026-04-01 11:19:26 +0200

    Añadir mensaje despedida y actualizamos el mensaje inicial

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-console.log("Hola mundo");
+console.log("Iniciando aplicación...");
 
 const saludo = (nombre) => {
     return `Hola, ${nombre}`;
+}
+
+const despedida = (nombre) =>{
+    return `Hasta luego, ${nombre}`
 }
